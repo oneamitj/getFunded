f33e95eb4ec95074ed776e7165b2109c6999459c
wannamit <wannamit@gmail.com>
2015-05-18 16:38:42 +0545

    Added 7 basic functions

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,47 @@
 class ProjectsController < ApplicationController
+	def index
+		@projects = Project.all
+	end
+
+	def new
+		@project = Project.new
+	end
+
+	def create
+		@project = Project.new(params[:project])
+		
+		if @project.save!
+			redirect_to projects_path
+		else
+			redirect_to :back
+		end
+	end
+
+	def show
+		@project = Project.find(params[:id])
+	end
+
+	def edit
+		@project = Project.find(params[:id])
+	end
+
+	def update
+		@project = Project.find(params[:id])
+		@project.update_attributes(params[:project])
+
+		if @project.save!
+			redirect_to projects_path
+		else
+			redirect_to :back
+		end
+	end
+
+	def destroy
+		Project.find(params[:id]).destroy
+
+		redirect_to projects_path
+	end
+
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -43,12 +43,12 @@ GetFunded::Application.routes.draw do
   #   namespace :admin do
   #     # Directs /admin/products/* to Admin::ProductsController
   #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
+      resources :projects
   #   end
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'projects#index'
 
   # See how all your routes lay out with "rake routes"
 
